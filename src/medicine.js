12352dba1f98b604efeaa56656d29d0f71a5f46e
Kardelen Duran <wildsnail0603@gmail.com>
2024-04-14 03:05:27 +0300

    Medicine Id
    Medicines were edited, removed or displayed by their indexes, now uuid is used instead.

--- src/medicine.js
+++ src/medicine.js
@@ -1,6 +1,6 @@
 class Medicine {
     constructor(id, name, dose) {
-      this.id = id; 
+      this.id = id; //Added id
       this.name = name;
       this.dose = dose;
       this.reminders = [];
